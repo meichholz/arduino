c8e4dee9daa7daf1e513795a23b8552fc060f38c
Marian Eichholz <marian.eichholz@freenet.ag>
2015-04-18 12:32:49 +0200

    fixed: key to freq mapping.

--- generate_speaker_tones.rb
+++ generate_speaker_tones.rb
@@ -1,9 +1,9 @@
 Keys = [ "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B" ]
-(0 .. 5).each do |octave|
-  (1 .. 12).each do |tone|
+(-1 .. 5).each do |octave|
+  (0 .. 11).each do |tone|
     printf "%d, // %s in %d\n",
-      (Math.exp(Math.log(2.0)*((tone-4+12*octave)/12.0)) *110.0).floor,
-        Keys[tone-1],
+      (Math.exp(Math.log(2.0)*((tone+3+12*octave)/12.0)) *110.0).floor,
+        Keys[tone],
         octave
   end
 
